3547e139b0df98068f9a830891d401d4b10c1ff8
pluselc <user@user-no-MacBook-Pro.local>
2016-12-22 01:42:25 +0900

    fix: mistake import path (to lowercase)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-import { HeroService } from './hero.Service';
+import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
