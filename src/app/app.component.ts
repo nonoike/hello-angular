994fb3fba9bc52ca7e7511a6e389614ede3f2772
pluselc <user@user-no-MacBook-Pro.local>
2016-12-24 19:43:47 +0900

    ch6: change styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
     selector: 'app-root',
     template: `
     <h1>{{title}}</h1>
-    <a routerLink="/dashboard">Dashboard</a>
-    <a routerLink="/heroes">Heroes</a>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
-    `
+    `,
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
